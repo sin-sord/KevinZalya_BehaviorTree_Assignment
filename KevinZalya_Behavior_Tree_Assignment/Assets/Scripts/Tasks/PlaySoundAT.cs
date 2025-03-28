3b4d2ce69d852b63a857534c5b68484a47a02cf6
sin-sord <kevkinzz@gmail.com>
2025-03-27 21:14:45 -0400

    Imported AudioManager

--- KevinZalya_Behavior_Tree_Assignment/Assets/Scripts/Tasks/PlaySoundAT.cs
+++ KevinZalya_Behavior_Tree_Assignment/Assets/Scripts/Tasks/PlaySoundAT.cs
@@ -1,14 +1,12 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
-using Unity.VisualScripting;
-using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
-	public class AttackAT : ActionTask {
+	public class PlaySoundAT : ActionTask {
 
-		public BBParameter<AudioSource> alertSound;
+		public string goblinSFX;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -20,7 +18,8 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			alertSound.value.Play();
+
+			AudioManager.Instance.PlaySound(goblinSFX);
 			EndAction(true);
 		}
 
